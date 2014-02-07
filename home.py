9f5218c54399fb1961d7b1fe3312842683ffa202
Yaw Boakye <wheresyaw@gmail.com>
2014-02-07 00:26:26 +0000

    added base template for constructing views
    What's the naming convention of Flask for widely used templates? Dunno,
    so I named it the way Rails does it. It already has Google Analytics
    monitoring visits, a Twitter follow button, and Bootstrap 3.1.0 for
    styling.

--- home.py
+++ home.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to home of all DevCongress #eXchanges"
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

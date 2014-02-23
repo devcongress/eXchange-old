8e693525f5739d13e67e18100916265d475ae956
Yaw Boakye <wheresyaw@gmail.com>
2014-02-23 00:35:50 +0000

    added functionality to register #eX participant

--- home.py
+++ home.py
@@ -7,6 +7,7 @@ from flask import (
                    flash
                    )
 from hashlib import sha256
+from bson import ObjectId
 from models import *
 
 # MongoDB settings (for development machine.)
@@ -18,8 +19,12 @@ app = Flask(__name__)
 app.secret_key = os.environ['EXCHANGE_SECRET']
 
 connection = connection or Connection(MONGODB_HOST, int(MONGODB_PORT))
+
 connection.register([Guest])
+connection.register([Participant])
+
 guests_collection = connection['exchange'].guests
+participants_collection = connection['exchange'].participants
 
 @app.route("/")
 def home():
@@ -31,7 +36,7 @@ def guests():
     #POST
     if request.method == 'POST':
         try:
-            register_prospective_exchanger(request.form)
+            register_member(guests_collection.Guest(), request.form)
             flash('We got that! Thanks for working to improve #eXchange')
             return redirect(url_for('home'))
         except:
@@ -58,10 +63,30 @@ def guest(name):
         return page_not_found()
 
 
+@app.route("/participants", methods=["POST"])
+def participants():
+    _id = ObjectId(request.form.get("exchange_id"))
+    exchanger = guests_collection.Guest.get_from_id(_id)
+    if exchanger:
+        try: register_member(participants_collection.Participant(), request.form)
+        except Exception: flash("Error. Your details could not be saved.")
+        else: flash("Great, we'd send you a Google Calendar invitation shortly.")
+        finally:
+            return render_template("guest.html", guest=exchanger)
+    return redirect(url_for("home"))
+
+def register_member(role, form_data):
+    for attr,value in form_data.iteritems():
+        role[attr] = value.strip()
+    print "DATABASE: %s, COLLECTION: %s successful." % (role.collection.database_name,
+                                                           role.collection.name)
+    role.save()
+
+
 def register_prospective_exchanger(form_data=None):
     guest = guests_collection.Guest()
     for attr,value in form_data.iteritems():
-        guest[attr] = value
+        guest[attr] = value.strip()
     guest.save()
 
 def find_guest_by(firstname, lastname):
--- models.py
+++ models.py
@@ -5,27 +5,48 @@ from datetime import datetime
 MONGOHQ_URL=os.environ.get('MONGOHQ_URL')
 connection = Connection(MONGOHQ_URL) if MONGOHQ_URL else None
 
+def min_length(limit):
+    def validate(value):
+        if value >= limit:
+            return True
+        raise Exception("%s should be at least %s characters." % (value,length))
+    return validate
+
+
+class Participant(Document):
+    '''A participant participates in exactly one event at a time. (I know this doesn't make sense.)'''
+    __database__ = "exchange"
+    __collection__ = "participants"
+
+    structure = {
+        "exchange_id": unicode, #TODO: better (or NoSQL-ic) foreign key?
+        "nickname"   : unicode,
+        "email"      : unicode,
+        "created_at" : datetime
+    }
+
+    required_fields = [ "nickname", "email", "exchange_id" ]
+    use_dot_notation = True
+
+    default_values = dict(created_at=datetime.utcnow)
+
+    validators = { "nickname": min_length(1), "email": min_length(5) }
+
+    def __repr__(self):
+        return "<#eXchange Participant {} <{}>>".format(self.nickname, self.email)
+
 class Guest(Document):
     '''A guest scheduled for an #exchange session.
        List of all guests are available at http://exchange.devcongress.com/guests
     '''
 
-    __database__ = 'exchange'
-    __collection__ = 'guests'
+    __database__ = "exchange"
+    __collection__ = "guests"
 
     def scheduled_date_after_now(scheduled_date):
         def validate(scheduled_date):
             return scheduled_date.timestamp() - datetime.now().timestamp() > 14*24*60*60
             raise Exception("Guests should be confirmed/suggest at least 2 weeks before their #eXchange is due.")
-
-    def min_length(length):
-      def validate(value):
-        if len(value.strip()) >= length:
-          return True
-        raise Exception("%s should be at least %s characters." % (value,length))
-
-      return validate
-
 
     structure = {
         "firstname"     : unicode,
@@ -50,10 +71,10 @@ class Guest(Document):
     }
 
     validators = {
-        'firstname'     : min_length(1),
-        'lastname'      : min_length(1),
-        'email_address' : min_length(1),
-        'bio'           : min_length(1)
+        "firstname"     : min_length(1),
+        "lastname"      : min_length(1),
+        "email_address" : min_length(1),
+        "bio"           : min_length(1)
     }
 
     use_dot_notation = True

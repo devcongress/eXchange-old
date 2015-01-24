179dcce3121b90c5ecbd2f9af6bd9634d2b7f223
Yaw Boakye <wheresyaw@gmail.com>
2015-01-24 02:13:27 +0000

    Strip out all Mongo stuff.
    Also delete all confusing code (aka code that I wrote a while back but
    don't exactly remember what it was supposed to do). Gives us the
    opportunity to write a new implementation with a better experience and
    no baggages. Isn't that what we want after all?

--- home.py
+++ home.py
@@ -1,63 +1,34 @@
+import os
+from datetime import datetime
 from flask import (
     Flask,
     url_for,
     render_template,
     redirect,
-    request,
-    flash
 )
-from hashlib import sha256
-from urlparse import urlparse
-from bson import ObjectId
-from models import *
 
 # Database configurations
 
-# MongoHQ on Heroku
-MONGOHQ_URL = os.environ.get('MONGOHQ_URL')
-
-if MONGOHQ_URL:
-    cred = urlparse(MONGOHQ_URL)
-    MONGODB_HOST = cred.hostname
-    MONGODB_PORT = int(cred.port)
-    MONGODB_USERNAME = cred.username
-    MONGODB_PASSWORD = cred.password
-    MONGODB_DATABASE = cred.path[1:]
-else:
-    # MongoDB settings (for development machine.)
-    MONGODB_HOST = os.environ.get('MONGODB_HOST')
-    MONGODB_PORT = int(os.environ.get('MONGODB_PORT'))
-    MONGODB_DATABASE = 'exchange'
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 # Update app's configuration.
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = os.environ['EXCHANGE_SECRET']
 
-db = MongoKit(app)
-db.register([Guest])
-db.register([Participant])
 
 @app.route("/")
 def home():
-    done = db.Guest.find({'scheduled_for': {'$lt': datetime.utcnow()}})
-    upcoming = db.Guest.find_one({'scheduled_for': {'$gt': datetime.utcnow()}})
+    done = []
+    upcoming = []
     return render_template('index.html', upcoming=upcoming, done=done)
 
-@app.route("/exchanges")
-@app.route("/guests", methods=['GET', 'POST'])
-def guests():
-    #POST
-    if request.method == 'POST':
-        try:
-            register_member('guests', request.form)
-            flash('We got that! Thanks for working to improve #eXchange')
-            return redirect(url_for('home'))
-        except Exception as e:
-            return render_template("new_guest.html")
 
-    #GET
+@app.route("/exchanges")
+@app.route("/guests")
+def guests():
     return redirect(url_for('home'))
+
 
 @app.route("/guests/<name>")
 def guest(name):
@@ -67,51 +38,14 @@ def guest(name):
 
     # real eXchangers and just some fool trying out endpoints
     try:
-        firstname, lastname = name.replace("-", " ").split()
-        print(firstname, lastname)
-        guest = find_guest_by(firstname, lastname)
-        if not guest:
-            raise Exception()
         return render_template('guest.html', guest=guest, now=datetime.utcnow())
-    except Exception as e:
-        print e
+    except Exception:
         return page_not_found()
 
-
-@app.route("/participants", methods=["POST"])
-def participants():
-    _id = ObjectId(request.form.get("exchange_id"))
-    exchanger = guests_collection.Guest.get_from_id(_id)
-    if exchanger:
-        try: register_member('participants', request.form)
-        except Exception: flash("Error. Your details could not be saved.")
-        else: flash("Great, we'd send you a Google Calendar invitation shortly.")
-        finally:
-            return render_template("guest.html", guest=exchanger)
-    return redirect(url_for("home"))
-
-def register_member(role, form_data):
-    stripped = { k:v.strip() for k,v in form_data.iteritems() }
-    db[role].insert(stripped)
-
-
-def find_guest_by(firstname, lastname):
-    return db['guests'].find_one(
-            {"firstname": firstname.title(), "lastname":lastname.title()}
-           )
-
-# model queries.
-
-def upcoming_exchanges():
-    return guests_collection.Guest.find({})
-def completed_exchanges(): pass
-
-# http errors
 
 @app.errorhandler(404)
 def page_not_found(error=None):
     return render_template('404.html', error=error), 404
 
-if __name__ == '__main__': app.run(debug=True)
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,92 +1,35 @@
-import os
-from flask.ext.mongokit import MongoKit, Document
 from datetime import datetime
 
-def min_length(limit):
-    def validate(value):
-        if value >= limit:
-            return True
-        raise Exception("%s should be at least %s characters." % (value,length))
-    return validate
 
-
-class Participant(Document):
-    '''A participant participates in exactly one event at a time. (I know this doesn't make sense.)'''
-    __collection__ = "participants"
-
-    structure = {
-        "exchange_id": unicode, #TODO: better (or NoSQL-ic) foreign key?
-        "nickname"   : unicode,
-        "email"      : unicode,
-        "created_at" : datetime
-    }
-
-    required_fields = [ "nickname", "email", "exchange_id" ]
-    use_dot_notation = True
-
-    default_values = dict(created_at=datetime.utcnow)
-
-    validators = { "nickname": min_length(1), "email": min_length(5) }
-
-    def __repr__(self):
-        return "<#eXchange Participant {} <{}>>".format(self.nickname, self.email)
-
-class Guest(Document):
-    '''A guest scheduled for an #exchange session.
-       List of all guests are available at http://exchange.devcongress.com/guests
+class Guest:
+    '''A guest scheduled for an #exchange session.  List of all guests are
+       available at http://exchange.devcongress.com/guests
     '''
 
-    __collection__ = "guests"
-
-    def scheduled_date_after_now(scheduled_date):
-        def validate(scheduled_date):
-            if scheduled_date.timestamp() - datetime.utcnow().timestamp() > 14*24*60*60:
-                return True
-            raise Exception("Guests should be confirmed/suggest at least 2 weeks before their #eXchange is due.")
-
     structure = {
-        "firstname"     : unicode,
-        "lastname"      : unicode,
-        "email_address" : unicode,
-        "homepage"      : unicode,
-        "bio"           : unicode,
-        "accepted"      : bool,
+        "firstname": unicode,
+        "lastname": unicode,
+        "email_address": unicode,
+        "homepage": unicode,
+        "bio": unicode,
+        "accepted": bool,
 
         # Github, and other social media usernames
-        "github"        : unicode,
-        "twitter"       : unicode,
-        "facebook"      : unicode,
-        "linkedin"      : unicode,
+        "github": unicode,
+        "twitter": unicode,
+        "facebook": unicode,
+        "linkedin": unicode,
 
         # Scheduling
-        "scheduled_for" : datetime,
-        "created_at"    : datetime,
-        "updated_at"    : datetime,
+        "scheduled_for": datetime,
+        "created_at": datetime,
+        "updated_at": datetime,
         "number_of_attendees": int,
         "actual_time_taken": float
     }
-
-    validators = {
-        "firstname"     : min_length(1),
-        "lastname"      : min_length(1),
-        "email_address" : min_length(1),
-        "bio"           : min_length(1)
-    }
-
-    use_dot_notation = True
-    required_fields = [
-                        "firstname",
-                        "lastname",
-                        "email_address",
-                        "bio"
-                      ]
-
-    default_values = dict(created_at=datetime.utcnow, updated_at=datetime.utcnow)
-
 
     def fullname(self):
         return "{} {}".format(self.firstname, self.lastname)
 
     def __repr__(self):
         return "<#exchange Guest (Name: {})>".format(self.fullname())
-

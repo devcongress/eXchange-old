fca753920a91dc1b25da6efe608a22cc5f349245
Yaw Boakye <wheresyaw@gmail.com>
2015-01-25 08:36:50 +0000

    Set secret key for development environment.
    Previously the secret key was read from the `EXCHANGE_SECRET`
    environment variable for all environments.

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 class Config:
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ.get('EXCHANGE_SECRET')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get("EXCHANGE_SECRET")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class ProductionConfig(Config):
@@ -15,3 +15,4 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEBUG = False
     DEVELOPMENT = True
+    SECRET_KEY = "a8814636fe767d1fbe7160dccbf35ee9238bc135add24c7256c360fb288ab22b4e78026a469ebb50336d7794d7864dc9ea0d95f58a8707efabaa34c7d0c6d5df9ca4deb4de3742c9d43b3016673fcb58"
